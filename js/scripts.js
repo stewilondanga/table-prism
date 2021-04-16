dbd117b63c1ce751ea4a0e6831942797eb5f532b
Stewart ilondanga <stilondanga@gmail.com>
2021-04-16 14:29:25 +0300

    add css/styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
-var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+/*var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+let transform = 0;
+let table = document.querySelector('table');
+let slides = table.querySelectorAll('tr').length;
+let singleRotation = 360 / slides;
+
+setInterval(() => {
+  table.style.setProperty('--rotate', `${-transform}deg`);
+  transform = transform + singleRotation;
+}, 1000);
